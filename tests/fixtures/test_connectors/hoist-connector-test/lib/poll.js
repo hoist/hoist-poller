52c86e8f48881b06ec8f79e0f01d4fd646d3fb5d
Amelia Laundy <amelialaundy@gmail.com>
2015-01-30 10:37:13 +1300

    adding app.js, application.json and setting the correct mongoose connection in eventbroker

--- tests/fixtures/test_connectors/hoist-connector-test/lib/poll.js
+++ tests/fixtures/test_connectors/hoist-connector-test/lib/poll.js
@@ -3,5 +3,5 @@ var BBPromise = require('bluebird');
 
 module.exports = function (app, bucket, subscription, bouncer, connector) {
   var eventName = connector.key + ':modified:invoice';
-  return subscription.eventEmitter.emit(eventName, arguments)
+  return subscription.eventEmitter.emit(eventName, {key: 'value'})
 }
